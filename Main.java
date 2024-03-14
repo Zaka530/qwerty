827f150c8cd6401a4091b989436ea9e0fc2e6d6e
kamron <kbariev7@gmail.com>
2024-03-14 15:01:56 +0500

    arraylist

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList numbers=new ArrayList();
+        numbers.addAll(Arrays.asList(2,5,4,4,4,8,7,6,27,5,4512,41,45));
+        System.out.println(numbers);
     }
 }

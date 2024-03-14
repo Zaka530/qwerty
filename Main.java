1356db313bf6e960e43315cb6916e0cc4e022402
kamron <kbariev7@gmail.com>
2024-03-14 18:24:54 +0500

    sort_arr

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        ArrayList numbers = new ArrayList();
+        numbers.addAll(Arrays.asList(2451, 15, 15, 11, 5, 45, 45, 41, 51, 51, 51, 51, 51, 51));
+        System.out.println(numbers);
 
-
-        Scanner in=new Scanner(System.in);
-        BigInteger size=new BigInteger(in.nextLine());
-        ArrayList numbers=new ArrayList();
-
-        for(BigInteger i=BigInteger.ZERO;i.compareTo(size)<0;i=i.add(BigInteger.ONE)){
-            numbers.add(i);
-            System.out.println(numbers);
-        }
+        List distinctNumbers = (List) numbers.stream().distinct().toList();
+        System.out.println(distinctNumbers);
     }
 }

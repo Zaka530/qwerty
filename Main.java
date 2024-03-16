2e4d843b2197b9dcd9bb9731bfcf0d18c6aff7cc
kamron <kbariev7@gmail.com>
2024-03-16 07:21:31 +0500

    !@#$

--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        ArrayList numbers = new ArrayList();
-        numbers.addAll(Arrays.asList(2451, 15, 15, 11, 5, 45, 45, 41, 51, 51, 51, 51, 51, 51));
-        System.out.println(numbers);
+       //lol
+        //ddd
 
-        List distinctNumbers = (List) numbers.stream().distinct().toList();
-        System.out.println(distinctNumbers);
+
+
+
+
     }
 }

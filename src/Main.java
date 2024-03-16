0f4f718ec4ecbf427c447d5145fa162a49425936
Zaka530 <kbariev7@gmail.com>
2024-03-16 11:17:01 +0500

    array123

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList numbers=new ArrayList();
+        numbers.addAll(Arrays.asList(23,234,565,46,543456,54345,65434,5654345));
+        System.out.println(numbers);
+    }
+}
